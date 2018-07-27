d47cdefe632ae21992b0764707465bc040d92b72
Jonas Bengtsson <jonas@bengtsson.cc>
2018-07-27 21:21:17 +0200

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from distutils.core import setup
 
 setup(name='autocrc',
-        version='1.4',
-        author='Jonas Bengtsson',
-        description = 'Automated CRC-checking',
-        author_email='jonasb@linux.se',
-        package_dir = {'autocrc' : 'src'},
-        packages = ['autocrc'],
-        url='http://sourceforge.net/projects/autocrc',
-        scripts = ['scripts/autocrc'],
-        license="GPLv3",
-        long_description = """
+      version='1.4',
+      author='Jonas Bengtsson',
+      description='Automated CRC-checking',
+      author_email='jonas@bengtsson.cc',
+      package_dir={'autocrc': 'src'},
+      packages=['autocrc'],
+      url='http://sourceforge.net/projects/autocrc',
+      scripts=['scripts/autocrc'],
+      license="GPLv3",
+      long_description="""
         autocrc uses 32-bit CRC-sums to verify the integrity of files.
         The CRC-sums are parsed both from filenames and from sfv files.
         autocrc can perform CRC-checks recursively. After it's done, it prints
